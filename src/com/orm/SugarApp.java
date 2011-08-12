ed68f1ee2c3a3dcf18bcce3bf56fab16f075c0a4
SATYA <satya@ubuntu.ubuntu-domain>
2011-08-12 20:15:56 +0530

    collection dsl

--- src/com/orm/SugarApp.java
+++ src/com/orm/SugarApp.java
@@ -1,12 +1,18 @@
 package com.orm;
 
+import java.util.HashSet;
+import java.util.Set;
+import static com.orm.dsl.Collection.set;
+
 public class SugarApp extends android.app.Application{
 
     SugarDb sugarDb;
+    Set<SugarDb> entities;
 
     public void onCreate(){
         super.onCreate();
         sugarDb = new SugarDb(this);
+        entities = set();
     }
 
     public void onTerminate(){
--- src/com/orm/SugarRecord.java
+++ src/com/orm/SugarRecord.java
@@ -4,6 +4,11 @@ import android.content.Context;
 
 public class SugarRecord<T> {
 
+    private Context context;
+
     public SugarRecord(Context context) {
+        this.context = context;
     }
+
+
 }
