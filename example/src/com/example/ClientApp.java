ea18cc8e84e8838f2ee928e431b73de85e0b494f
Jan Paul Imhoff <jan-paul.imhoff@sixt.com>
2014-02-20 10:35:09 +0100

    Modified the Example to be in line with the new startup system

---
+++ example/src/com/example/ClientApp.java
@@ -0,0 +1,21 @@
+package com.example;
+
+import com.orm.SugarContext;
+
+import android.app.Application;
+
+public class ClientApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        SugarContext.init(this);
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+        SugarContext.terminate();
+    }
+
+}
