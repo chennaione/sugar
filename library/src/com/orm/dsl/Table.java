1cea42dfd72c737b7010c849e49819fc0fdc7eda
Daniel Arsenault <daniel.arsenault@dasoft.ca>
2014-07-21 22:03:05 -0500

    Added Table annotation for custom naming of tables

---
+++ library/src/com/orm/dsl/Table.java
@@ -0,0 +1,10 @@
+package com.orm.dsl;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Table
+{
+    String name();
+}
