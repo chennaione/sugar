6370b6067fb9ce6096dfce658d43eb96949f87a7
Huu Nguyen <whoshuu@gmail.com>
2015-03-31 08:05:51 -0700

    Add IncompleteAnnotatedModel and test

---
+++ example/src/test/java/com/example/sugartest/IncompleteAnnotatedModelTests.java
@@ -0,0 +1,21 @@
+package com.example.sugartest;
+
+
+import android.database.sqlite.SQLiteException;
+
+import com.example.models.IncompleteAnnotatedModel;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.annotation.Config;
+
+import static com.orm.SugarRecord.save;
+
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(emulateSdk=18)
+public class IncompleteAnnotatedModelTests {
+    @Test(expected=SQLiteException.class)
+    public void noIdFieldTest() {
+        save(new IncompleteAnnotatedModel());
+    }
+}
