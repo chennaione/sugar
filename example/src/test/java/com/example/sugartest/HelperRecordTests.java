c304fce8bf6cf83ad4fe80c6ffc11ccc27510af0
Chris Desch <cdesch@gmail.com>
2015-04-01 12:29:34 -0400

    Fixing helper methods and test cases
    Added test cases for the use case of the table having deleted elements.
    Changed the helper methods first() and last() to use findByQuery() instead of index.
    All tests are passing.
    
    Signed-off-by: Chris Desch <cdesch@gmail.com>

--- example/src/test/java/com/example/sugartest/HelperRecordTests.java
+++ example/src/test/java/com/example/sugartest/HelperRecordTests.java
@@ -18,13 +18,29 @@ import static org.junit.Assert.assertFalse;
 public class HelperRecordTests {
     @Test
     public void firstHelperTest() {
-
         Float firstObjectFloat = new Float(25F);
         Float lastObjectFloat = new Float(50F);
         save(new FloatFieldExtendedModel(firstObjectFloat));
         save(new FloatFieldExtendedModel(lastObjectFloat));
-        FloatFieldExtendedModel model = SugarRecord.first(FloatFieldExtendedModel.class);
+        FloatFieldExtendedModel model = FloatFieldExtendedModel.first(FloatFieldExtendedModel.class);
         assertEquals(firstObjectFloat, model.getFloat() );
+    }
+
+    @Test
+    public void firstHelperDeletedRecordTest() {
+        System.out.println("bla");
+        Float firstObjectFloat = new Float(15F);
+        Float secondObjectFloat = new Float(25F);
+        Float thirdObjectFloat = new Float(35F);
+        Float fourthObjectFloat = new Float(45F);
+        save(new FloatFieldExtendedModel(firstObjectFloat));
+        save(new FloatFieldExtendedModel(secondObjectFloat));
+        save(new FloatFieldExtendedModel(thirdObjectFloat));
+        save(new FloatFieldExtendedModel(fourthObjectFloat));
+        FloatFieldExtendedModel firstRecord = FloatFieldExtendedModel.findById(FloatFieldExtendedModel.class,1);
+        firstRecord.delete();
+        FloatFieldExtendedModel model = FloatFieldExtendedModel.first(FloatFieldExtendedModel.class);
+        assertEquals(secondObjectFloat, model.getFloat() );
     }
 
     @Test
@@ -33,27 +49,43 @@ public class HelperRecordTests {
         Float lastObjectFloat = new Float(50F);
         save(new FloatFieldExtendedModel(firstObjectFloat));
         save(new FloatFieldExtendedModel(lastObjectFloat));
-        FloatFieldExtendedModel model = SugarRecord.last(FloatFieldExtendedModel.class);
+        FloatFieldExtendedModel model = FloatFieldExtendedModel.last(FloatFieldExtendedModel.class);
         assertEquals(lastObjectFloat, model.getFloat() );
     }
 
     @Test
+    public void lastHelperDeletedRecordTest() {
+        Float firstObjectFloat = new Float(15F);
+        Float secondObjectFloat = new Float(25F);
+        Float thirdObjectFloat = new Float(35F);
+        Float fourthObjectFloat = new Float(45F);
+        save(new FloatFieldExtendedModel(firstObjectFloat));
+        save(new FloatFieldExtendedModel(secondObjectFloat));
+        save(new FloatFieldExtendedModel(thirdObjectFloat));
+        save(new FloatFieldExtendedModel(fourthObjectFloat));
+        FloatFieldExtendedModel lastRecord = FloatFieldExtendedModel.findById(FloatFieldExtendedModel.class, 4);
+        lastRecord.delete();
+        FloatFieldExtendedModel model = FloatFieldExtendedModel.last(FloatFieldExtendedModel.class);
+        assertEquals(thirdObjectFloat, model.getFloat());
+    }
+
+    @Test
     public void nullFirstHelperTest() {
-        FloatFieldExtendedModel model = SugarRecord.first(FloatFieldExtendedModel.class);
+        FloatFieldExtendedModel model = FloatFieldExtendedModel.first(FloatFieldExtendedModel.class);
         assertNull(model);
     }
 
     @Test
     public void nullLastHelperTest() {
-        FloatFieldExtendedModel model = SugarRecord.last(FloatFieldExtendedModel.class);
+        FloatFieldExtendedModel model = FloatFieldExtendedModel.last(FloatFieldExtendedModel.class);
         assertNull(model);
     }
 
     @Test
     public void oneItemHelperTest() {
         save(new FloatFieldExtendedModel(new Float(25F)));
-        FloatFieldExtendedModel firstModel = SugarRecord.first(FloatFieldExtendedModel.class);
-        FloatFieldExtendedModel lastModel = SugarRecord.last(FloatFieldExtendedModel.class);
+        FloatFieldExtendedModel firstModel = FloatFieldExtendedModel.first(FloatFieldExtendedModel.class);
+        FloatFieldExtendedModel lastModel = FloatFieldExtendedModel.last(FloatFieldExtendedModel.class);
         assertEquals(firstModel.getFloat(), lastModel.getFloat());
     }
 }
