8d997ba8ed62f49366dfdb42737d8d1f345b0809
Nursultan Turdaliev <nursultan2010@gmail.com>
2015-12-06 22:51:28 +0100

    NumberComparator tests

---
+++ library/src/test/java/com/orm/util/NumberComparatorTest.java
@@ -0,0 +1,33 @@
+package com.orm.util;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by nursultan on 12/6/15.
+ */
+public class NumberComparatorTest {
+
+    private String ten;
+    private String eleven;
+    private NumberComparator numberComparator;
+
+    @Before
+    public void setUp() throws Exception {
+
+        ten = "10.sql";
+        eleven = "11.sql";
+        numberComparator =  new NumberComparator();
+
+    }
+
+    @Test
+    public void testCompare() throws Exception {
+
+        assertEquals("Testing a small number with a big number",-1,numberComparator.compare(ten,eleven));
+        assertEquals("Testing equal numbers",0,numberComparator.compare(ten, ten));
+        assertEquals("Testing a big number with a small number  ",1,numberComparator.compare(eleven,ten));
+    }
+}
