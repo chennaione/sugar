297a8381e8a7c4bef9a1875ea6c1a2d6786053b7
Sibelius Seraphini <sibeliusseraphini@gmail.com>
2015-12-11 15:09:25 -0200

    removing static from callback interface

--- library/src/main/java/com/orm/SugarTransactionHelper.java
+++ library/src/main/java/com/orm/SugarTransactionHelper.java
@@ -24,7 +24,7 @@ public class SugarTransactionHelper {
         }
     }
 
-    public static interface Callback {
+    public interface Callback {
         void manipulateInTransaction();
     }
 }
