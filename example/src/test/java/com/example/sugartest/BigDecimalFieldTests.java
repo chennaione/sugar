92fc2ca52bc4159e0ca0e8f50968e4ce0ffdf721
Diogo Queiroz <diogosq@gmail.com>
2016-01-07 23:39:03 -0200

    ADD JUnit BigDecimal using pk notation and table notation without id field

--- example/src/test/java/com/example/sugartest/BigDecimalFieldTests.java
+++ example/src/test/java/com/example/sugartest/BigDecimalFieldTests.java
@@ -3,6 +3,7 @@ package com.example.sugartest;
 
 import com.example.models.BigDecimalFieldAnnotatedModel;
 import com.example.models.BigDecimalFieldExtendedModel;
+import com.example.models.PrimaryKeyNotationBigDecimalFieldAnnotatedModel;
 import com.orm.SugarRecord;
 
 import org.junit.Test;
@@ -48,6 +49,31 @@ public class BigDecimalFieldTests {
         save(new BigDecimalFieldAnnotatedModel(decimal));
         BigDecimalFieldAnnotatedModel model =
                 SugarRecord.findById(BigDecimalFieldAnnotatedModel.class, 1);
+        assertEquals(decimal, model.getBigDecimal());
+    }
+    @Test
+    public void primryKeyNotationNullBigDecimalExtendedTest() {
+        save(new PrimaryKeyNotationBigDecimalFieldAnnotatedModel());
+        PrimaryKeyNotationBigDecimalFieldAnnotatedModel model =
+                SugarRecord.findById(PrimaryKeyNotationBigDecimalFieldAnnotatedModel.class, 1);
+        assertNull(model.getBigDecimal());
+    }
+
+    @Test
+    public void primryKeyNotationBigDecimalExtendedTest() {
+        BigDecimal decimal = new BigDecimal(1234.5678901234567890123456789);
+        save(new PrimaryKeyNotationBigDecimalFieldAnnotatedModel(decimal));
+        PrimaryKeyNotationBigDecimalFieldAnnotatedModel model =
+                SugarRecord.findById(PrimaryKeyNotationBigDecimalFieldAnnotatedModel.class, 1);
+        assertEquals(decimal, model.getBigDecimal());
+    }
+
+    @Test
+    public void primryKeyNotationBigDecimalAnnotatedTest() {
+        BigDecimal decimal = new BigDecimal(1234.5678901234567890123456789);
+        save(new PrimaryKeyNotationBigDecimalFieldAnnotatedModel(decimal));
+        PrimaryKeyNotationBigDecimalFieldAnnotatedModel model =
+                SugarRecord.findById(PrimaryKeyNotationBigDecimalFieldAnnotatedModel.class, 1);
         assertEquals(decimal, model.getBigDecimal());
     }
 }
