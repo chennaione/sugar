5690f92320980674389cac4669fec553f321dec5
Tevin Jeffrey <mastermindtj94@gmail.com>
2016-01-12 11:06:43 -0500

    Added the @Target annotation for compile-time error checking.
    - The compiler will generate an error when used on the improper element
    type.

--- library/src/main/java/com/orm/dsl/Table.java
+++ library/src/main/java/com/orm/dsl/Table.java
@@ -1,9 +1,12 @@
 package com.orm.dsl;
 
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
 public @interface Table {
     String name() default "";
 }
