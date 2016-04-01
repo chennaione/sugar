8b8005718a43bd242802f954649fe87cc5f3ebf1
geracervantes <gcervantes@nearsoft.com>
2016-03-31 17:37:50 -0700

    fix error

--- library/src/main/java/com/orm/SchemaGenerator.java
+++ library/src/main/java/com/orm/SchemaGenerator.java
@@ -177,7 +177,7 @@ public class SchemaGenerator {
     }
 
     protected String createTableSQL(Class<?> table) {
-        KeyWord link = new KeyWord;
+        KeyWord link = new KeyWord();
         Log.i(SUGAR, "Create table if not exists");
         List<Field> fields = ReflectionUtil.getTableFields(table);
         String tableName = NamingHelper.toSQLName(table);
