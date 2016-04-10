6b3182c859040a4034a138a7c9686f657a26e6cc
jonatan <jonatansalas@live.com.ar>
2016-04-10 18:31:19 -0300

    Added Test for SugarConfig

--- library/src/test/java/com/orm/util/SugarConfigTest.java
+++ library/src/test/java/com/orm/util/SugarConfigTest.java
@@ -1,8 +1,27 @@
 package com.orm.util;
 
+import com.orm.model.TestRecord;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @author jonatan.salas
  */
-public class SugarConfigTest {
+public final class SugarConfigTest {
 
+    @Test
+    public void testSetGetFields() {
+        Field[] fields = TestRecord.class.getFields();
+
+        List<Field> fieldList = Arrays.asList(fields);
+        SugarConfig.setFields(TestRecord.class, fieldList);
+
+        Assert.assertEquals(fieldList, SugarConfig.getFields(TestRecord.class));
+    }
 }
