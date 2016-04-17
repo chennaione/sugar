d9bceb06ec4bbd556d4f3be7272b1f6bea74909b
jonatan <jonatansalas@live.com.ar>
2016-04-17 19:21:25 -0300

    Added ThreadUtil documentation

--- library/src/main/java/com/orm/util/ThreadUtil.java
+++ library/src/main/java/com/orm/util/ThreadUtil.java
@@ -6,6 +6,8 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
 /**
+ * Util class to deal with threads.
+ *
  * @author jonatan.salas
  */
 public final class ThreadUtil {
@@ -13,6 +15,12 @@ public final class ThreadUtil {
     //Prevent instantiation..
     private ThreadUtil() { }
 
+    /**
+     * Submits a Callable object and returns a Future ready to use.
+     *
+     * @param callable the callable you want to submit
+     * @return a Future object
+     */
     public static Future doInBackground(Callable callable) {
         final ExecutorService executor = Executors.newSingleThreadExecutor();
         Future future = executor.submit(callable);
