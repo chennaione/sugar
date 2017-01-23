a1cc85347e696e8be6b418f3c587c32b8272632d
Matthew Trout <matthew.trout@bespokesoftware.com>
2017-01-23 13:24:33 +0000

    tests for getFieldByName and getAllFields

---
+++ library/src/test/java/com/orm/model/SimpleModelWithMore.java
@@ -0,0 +1,14 @@
+package com.orm.model;
+
+public class SimpleModelWithMore extends SimpleModel {
+
+    private String more;
+
+    public String getMore() {
+        return more;
+    }
+
+    public void setMore(String more) {
+        this.more = more;
+    }
+}
